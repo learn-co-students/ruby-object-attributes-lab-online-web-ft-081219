35c12cef9c9f53b6c6c7a5e90057c83d667834f0
Guillermo Martinez <dumblemoe@gmail.com>
2019-08-19 23:09:15 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog 
+def name=(good_bois_name)
+    @name = good_bois_name
+ end
+def name 
+   @name 
+  end
+def breed=(what_am_I)
+    @breed = what_am_I
+  end
+def breed 
+  @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name=(i_am_me)
+    @name = i_am_me
+  end
+  def name 
+    @name 
+  end
+  def job=(i_hate_this_job)
+    @job = i_hate_this_job
+  end
+  def job
+    @job 
+  end
+end
