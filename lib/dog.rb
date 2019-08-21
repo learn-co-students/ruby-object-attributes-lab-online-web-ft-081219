19aa13579417d291b722c65362fb09a2917fd6cd
K Hong <albtrossfacefall@gmail.com>
2019-08-21 04:04:37 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  
+  def name=(read_name)
+    @name=read_name
+  end  
+  
+  def name
+    @name
+  end
+  
+  def breed=(read_breed)
+    @breed=(read_breed)
+  end
+  
+  def breed
+    @breed
+  end 
+  
+end
+
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  
+  def name=(read_name)
+    @name=read_name
+  end
+
+  def name
+    @name
+  end
+  
+  def job=(read_job)
+    @job=read_job
+  end
+  
+  def job
+    @job
+  end  
+  
+end
