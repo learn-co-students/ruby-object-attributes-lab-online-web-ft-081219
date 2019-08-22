4fc83a9b8f24e11b8a29dc72f58f04902e10e71a
Evan Jeffries <ejeffries14@gmail.com>
2019-08-22 21:55:46 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name=(new_dog)
+    @name = new_dog
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+  
+  def breed
+    @breed
+  end
+end
